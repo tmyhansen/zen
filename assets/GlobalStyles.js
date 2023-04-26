1a31dda398cb9457e8c1e01ce060ee60942039b8
tmyhansen <tommyh40@gmail.com>
2023-04-26 23:39:03 +0200

    Add files via upload

---
+++ assets/GlobalStyles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+const globalStyles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    fontFamily: 'Wingdings' // set your desired font family here
+  }
+});
+
+export default globalStyles;
